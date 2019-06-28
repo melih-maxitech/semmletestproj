4b6b5114b2dea71339316c039e40d38f0bc706b0
Melih T. Tolan <tuncay.tolan@softtech.com.tr>
2019-06-28 16:04:26 -0700

    asdas

--- src/main/java/com/semmletestproj/Bus.java
+++ src/main/java/com/semmletestproj/Bus.java
@@ -1,8 +1,17 @@
 package com.semmletestproj;
 
-public class Bus extends Vehicles {
+import java.io.Serializable;
+
+public class Bus extends Vehicles implements Serializable {
 
     public String name = "Bus";
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        return "12312312".equals(((String) o).substring(1));
+    }
 
 }
